965b05ed7999d61a035d7621ea900839d3c5766f
LeastKIds <leastkids@gmail.com>
2025-03-24 14:48:44 +0900

    refactor: update Profile struct fields with new metadata and types

--- entity/profile.go
+++ entity/profile.go
@@ -3,9 +3,9 @@ package entity
 // @db
 // @name {프로필}
 type Profile struct {
-	ID     int    `json:"id" db:"id" reference:"아이디3"`
-	UserID int    `json:"user_id" db:"user_id" reference:"유저아이디"`
-	Phone  string `json:"phone" db:"phone" reference:"전화번호"`
-	Email  string `json:"email" db:"email" reference:"이메일1"`
-	Age    int    `json:"age" db:"age" reference:"나이"`
+	ID     int    `no:"1" column:"id" name:"아이디" type:"BIGINT" pk:"O" fk:"" null:"X" index:"" reference:"자동생성"`
+	UserID int    `no:"2" column:"user_id" name:"유저 아이디" type:"BIGINT" pk:"" fk:"O" null:"X" index:"" reference:"유저 아이디"`
+	Phone  string `no:"3" column:"phone" name:"전화번호" type:"VARCHAR(255)" pk:"" fk:"" null:"X" index:"" reference:"전화번호"`
+	Email  string `no:"4" column:"email" name:"이메일" type:"VARCHAR(255)" pk:"" fk:"" null:"X" index:"" reference:"이메일"`
+	Age    int    `no:"5" column:"age" name:"나이" type:"INT" pk:"" fk:"" null:"X" index:"" reference:"나이"`
 }
